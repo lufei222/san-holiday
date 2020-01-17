7b28cbc961ea9f872ce3d901cb36e4a543fbc28f
luosan <456zxcvbnm>
2020-01-17 14:20:55 +0800

    提交2020版本的更新

--- src/main/java/GetHolidays.java
+++ src/main/java/GetHolidays.java
@@ -4,8 +4,6 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -16,12 +14,8 @@ public class GetHolidays {
     private  static String SEPARATOR = "";
     private static int YEAR = 2020;
     static SimpleDateFormat yyyyMMddFMT = new SimpleDateFormat("yyyyMMdd") ;
-
-
-
     /**
-     *         全年假期集合 = [查询全年的双休周末] + [得到所有的法定节假日] - [得到所有的调休日] - [查询数据库已经存在的假期集合]
-     * @param args
+     * 全年假期集合 = [查询全年的双休周末] + [得到所有的法定节假日] - [得到所有的调休日] - [查询数据库已经存在的假期集合]
      */
     public static void main(String[] args) {
 
@@ -145,90 +139,6 @@ public class GetHolidays {
         DBhepler.batchInsert(list,sql);
     }
 
-    /***
-     * 得到一年内所有日期
-     * @param year
-     * @return
-     */
-    public static List<String> getAllDaysByYear(int year) {
-        return getAllDays(year+"-01-01", year+"-12-31");
-    }
-
-    /***
-     * 得到指定区间内的所有日期
-     * @param startTime
-     * @param endTime
-     * @return
-     */
-    public static List<String> getAllDays(String startTime, String endTime) {
-        /**
-         * 获取两个日期之间的所有日期
-         *
-         * @param startTime
-         *            开始日期
-         * @param endTime
-         *            结束日期
-         * @return
-         */
-            // 返回的日期集合
-            List<String> days = new ArrayList<String>();
-
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            try {
-                Date start = dateFormat.parse(startTime);
-                Date end = dateFormat.parse(endTime);
-
-                Calendar tempStart = Calendar.getInstance();
-                tempStart.setTime(start);
-
-                Calendar tempEnd = Calendar.getInstance();
-                tempEnd.setTime(end);
-                tempEnd.add(Calendar.DATE, +1);// 日期加1(包含结束)
-                while (tempStart.before(tempEnd)) {
-                    String formatLineDay = dateFormat.format(tempStart.getTime());
-                    String formatDat = formatLineDay.replaceAll("-","");
-                    days.add(formatDat);
-                    tempStart.add(Calendar.DAY_OF_YEAR, 1);
-                }
-
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-            return days;
-    }
-
-    /**
-     * 获取一年内所有的双休日,这个方法有问题，[20190832, 20191131]竟然有这两个非法日期
-     * @param year
-     * @return
-     */
-    @Deprecated
-    public static Set<String> getYearDoubleWeekend(int year){
-        Set<String> listDates = new HashSet<String>();
-        Calendar calendar=Calendar.getInstance();//当前日期
-        calendar.set(year, 6, 1);
-        Calendar nowyear=Calendar.getInstance();
-        Calendar nexty=Calendar.getInstance();
-        nowyear.set(year, 0, 1);//2010-1-1
-        nexty.set(year+1, 0, 1);//2011-1-1
-        calendar.add(Calendar.DAY_OF_MONTH, -calendar.get(Calendar.DAY_OF_WEEK));//周六
-        Calendar c=(Calendar) calendar.clone();
-        for(;calendar.before(nexty)&&calendar.after(nowyear);calendar.add(Calendar.DAY_OF_YEAR, -7)){
-            listDates.add(calendar.get(Calendar.YEAR) + SEPARATOR + getDoubleMonthStr((1+calendar.get(Calendar.MONTH))) + SEPARATOR + getDoubleMonthStr(calendar.get(Calendar.DATE)));
-            listDates.add(calendar.get(Calendar.YEAR) + SEPARATOR + getDoubleMonthStr(1+calendar.get(Calendar.MONTH)) + SEPARATOR + getDoubleMonthStr(1+calendar.get(Calendar.DATE)));
-        }
-        for(;c.before(nexty)&&c.after(nowyear);c.add(Calendar.DAY_OF_YEAR, 7)){
-            listDates.add(c.get(Calendar.YEAR) + SEPARATOR + getDoubleMonthStr(1+c.get(Calendar.MONTH)) + SEPARATOR + getDoubleMonthStr(c.get(Calendar.DATE)));
-            listDates.add(c.get(Calendar.YEAR) + SEPARATOR + getDoubleMonthStr(1+c.get(Calendar.MONTH)) + SEPARATOR + getDoubleMonthStr(1+c.get(Calendar.DATE)));
-        }
-        return listDates;
-    }
-    private static String getDoubleMonthStr(int monthInt){
-        String monthStr = monthInt+"";
-        String doublemonth = monthStr.length()==1? "0"+ monthStr : monthStr ;
-        return doublemonth;
-    }
     public static Set<String> getWeekDayList(int year) {
         Set<String> listDate = new HashSet<>();
         int i = 1;
@@ -283,7 +193,7 @@ public class GetHolidays {
                 });
             }
         }
-        System.out.println("工作日-------->");
+        System.out.println("上班日-------->");
         Collections.sort(workDays);
         workDays.forEach(x->{
             System.out.println(x);
